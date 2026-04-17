d140fd330932281d3c695cc8b2a1446195b121de
fajhrinazgul <fathfajri843@gmail.com>
2026-04-17 20:30:34 +0700

    update post create, and add handler GET

--- posthandler.go
+++ posthandler.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/fajhrinazgul/mysite-api/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
+// createPostHandler is handler to create new post
 func createPostHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		start := time.Now()
+
 		_, err := getUserContext(ctx.Request)
 		if err != nil {
 			ctx.JSON(http.StatusUnauthorized, gin.H{
@@ -70,10 +76,58 @@ func createPostHandler() gin.HandlerFunc {
 			return
 		}
 
+		duration := time.Since(start)
+
 		ctx.JSON(http.StatusCreated, gin.H{
-			"status":  "success",
-			"message": "success create new post",
-			"post":    post,
+			"status":         "success",
+			"message":        "success create new post",
+			"post":           post,
+			"execution_time": float64(duration.Microseconds()) / 1000,
+		})
+	}
+}
+
+func getAllPostHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		start := time.Now()
+		page, _ := strconv.Atoi(ctx.DefaultQuery("page", "1"))
+		limit, _ := strconv.Atoi(ctx.DefaultQuery("limit", "10"))
+		status := ctx.DefaultQuery("status", "published")
+
+		offset := (page - 1) * limit
+
+		postModel := models.NewPostModel(models.GetDB())
+		posts, total, err := postModel.GetPagedPosts(status, limit, offset)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"status":  "error",
+				"message": "Failed to fetch data",
+			})
+			return
+		}
+
+		totalPage := int(math.Ceil(float64(total) / float64(limit)))
+
+		duration := time.Since(start)
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"status":         "success",
+			"message":        "Posts retrieve successfully",
+			"data":           posts,
+			"execution_time": float64(duration.Microseconds()) / 1000,
+			"pagination": gin.H{
+				"current_page": page,
+				"limit":        limit,
+				"total_items":  total,
+				"total_page":   totalPage,
+				"has_next":     page < totalPage,
+				"has_prev":     page > 1,
+			},
+			"queries": gin.H{
+				"limit":  limit,
+				"offset": offset,
+				"status": status,
+			},
 		})
 	}
 }
